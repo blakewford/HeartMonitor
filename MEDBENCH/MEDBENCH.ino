456a98577394bee49e7a454537f1929cbce3deb9
Blake W. Ford <blakewford@gmail.com>
2018-09-16 13:08:34 -0500

    Start transistion to codegen experiment.

--- MEDBENCH/MEDBENCH.ino
+++ MEDBENCH/MEDBENCH.ino
@@ -6,62 +6,130 @@ Sprites sprites;
 Arduboy2Base arduboy;
 ArduboyTones sound(arduboy.audio.enabled);
 
-void setup ()
+struct frameInfo
 {
-    arduboy.begin();
-    arduboy.setFrameRate(60);
-    arduboy.initRandomSeed();
+    uint16_t* count;
+    void (*method)(uint16_t*); 
+
+    frameInfo(uint16_t value, void (*function)(uint16_t*))
+    {
+        count = new uint16_t(value);
+        method = function;
+    }
+};
+
+uint8_t gCurrentFrame = 0;
+frameInfo* gFrames = (frameInfo*)malloc(sizeof(frameInfo)*256);
+void drawImage(const unsigned char* image, uint16_t* count)
+{
+    if (!(arduboy.nextFrame())) return;
+    if((*count)--)
+    {   
+        sprites.drawSelfMasked(0, 0, image, 0);
+        arduboy.display();
+    }
+    else
+    {
+        arduboy.clear();
+        gCurrentFrame++;
+    }
+}
+
+void nop(uint16_t* count)
+{
+    if (!(arduboy.nextFrame())) return;
+    if((*count)--)
+    {
+    }
+    else
+    {
+        gCurrentFrame++;
+    }
+}
+
+void frame0(uint16_t* count)
+{
+    drawImage(Logo, count);   
+}
+
+void frame1(uint16_t* count)
+{
+    drawImage(Heart, count); 
 }
 
 uint8_t gCounter = 0;
 uint8_t gVariable = 0;
 uint16_t gAccumulator = 0;
-int32_t gFrameCounter = 0;
-void loop()
+
+void frame2(uint16_t* count)
 {
     const uint8_t bpmX = 94;
     const uint8_t bpmY = 32;
 
-    if (!(arduboy.nextFrame())) return;
+    nop(count);
+    
+    gVariable = random(0, 9);
+    gAccumulator += gVariable;
+    gCounter++;
 
-    gFrameCounter++;
-    arduboy.pollButtons();
     arduboy.clear();
 
-    if(gFrameCounter < 180)
+    sprites.drawSelfMasked(bpmX, bpmY, Numbers, 8);
+    sprites.drawSelfMasked(bpmX + 5, bpmY, Numbers, gVariable);
+    sprites.drawSelfMasked(bpmX-20, bpmY, BPM, 0);     
+}
+
+void frame3(uint16_t* count)
+{
+    const uint8_t bpmX = 94;
+    const uint8_t bpmY = 32;
+
+    drawImage(Heart, count);
+    gVariable = random(0, 9);
+    gAccumulator += gVariable;
+    gCounter++;
+}
+
+void frame4(uint16_t* count)
+{
+    nop(count);
+    if(*count != 0)
     {
-        sprites.drawSelfMasked(0, 0, Logo, 0);      
+        *gFrames[2].count = 20;
+        *gFrames[3].count = 20;
+        gCurrentFrame = 2;
     }
-    else if(gFrameCounter < 360) //TODO button input
-    {
-        sprites.drawSelfMasked(0, 0, Heart, 0);      
-    }    
-    else if(gFrameCounter%60 && gFrameCounter < 960)
-    {
-        if(gFrameCounter%60 < 20)
-        {
-            sprites.drawSelfMasked(0, 0, Heart, 0);
-            gVariable = random(0, 9);
-            gAccumulator += gVariable;
-            gCounter++;
-        }
-    }
-    else if(gFrameCounter > 960) //TODO play sound
-    {
-        gVariable = gAccumulator / gCounter;
-        sprites.drawSelfMasked(0, 0, Heart, 0);  
-    }      
     else
     {
-        sprites.drawSelfMasked(0, 0, Heart, 0);   
+        gCurrentFrame = 5;
     }
+}
 
-    if(gFrameCounter > 360)
+void setup ()
+{
+    arduboy.begin();
+    arduboy.setFrameRate(60);
+    arduboy.initRandomSeed();
+
+    frameInfo info0(180, &frame0);
+    frameInfo info1(180, &frame1);
+    frameInfo info2(20, &frame2);
+    frameInfo info3(20, &frame3);
+    frameInfo info4(20, &frame4);
+    gFrames[0] = info0;
+    gFrames[1] = info1;
+    gFrames[2] = info2;
+    gFrames[3] = info3;
+    gFrames[4] = info4;
+}
+
+#define TOTAL_FRAMES 5
+
+void loop()
+{
+    if(gCurrentFrame < TOTAL_FRAMES)
     {
-        sprites.drawSelfMasked(bpmX, bpmY, Numbers, 8);
-        sprites.drawSelfMasked(bpmX + 5, bpmY, Numbers, gVariable);
-        sprites.drawSelfMasked(bpmX-20, bpmY, BPM, 0); 
+        void (*frame)(uint16_t*) = (void (*)(uint16_t*))gFrames[gCurrentFrame].method;
+        (*frame)(gFrames[gCurrentFrame].count);
     }
-
-    arduboy.display();
 }
