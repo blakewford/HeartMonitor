01136560445de4aa0d3d05920e1406db7e106430
Blake W. Ford <blakewford@gmail.com>
2018-09-22 16:11:30 -0500

    Move more towards autogenerate model.

--- MEDBENCH/MEDBENCH.ino
+++ MEDBENCH/MEDBENCH.ino
@@ -20,17 +20,17 @@ struct frameInfo
 
 uint8_t gCurrentFrame = 0;
 frameInfo* gFrames = (frameInfo*)malloc(sizeof(frameInfo)*256);
-void drawImage(const unsigned char* image, uint16_t* count)
+void drawImage(const unsigned char* image, uint16_t* count, uint8_t x, uint8_t y, bool autoClear, uint8_t frame)
 {
     if (!(arduboy.nextFrame())) return;
     if((*count)--)
     {   
-        sprites.drawSelfMasked(0, 0, image, 0);
+        sprites.drawSelfMasked(x, y, image, frame);
         arduboy.display();
     }
     else
     {
-        arduboy.clear();
+        if(autoClear) arduboy.clear();
         gCurrentFrame++;
     }
 }
@@ -52,63 +52,99 @@ void waitFrame(uint16_t* count)
     nop(count);
 }
 
-void frame0(uint16_t* count)
+void randomizeVariable(uint8_t* variable, long small, long big)
 {
-    drawImage(Logo, count);   
+    *variable = random(small, big);
 }
 
-void frame1(uint16_t* count)
+void addToVariable(uint8_t* variable, uint8_t value)
 {
-    drawImage(Heart, count); 
+    *variable += value; 
 }
+
+void addToVariable(uint16_t* variable, uint8_t value)
+{
+    *variable += value;     
+}
+
+#define TOTAL_FRAMES 8
+
+void loop()
+{
+    if(gCurrentFrame < TOTAL_FRAMES)
+    {
+        void (*frame)(uint16_t*) = (void (*)(uint16_t*))gFrames[gCurrentFrame].method;
+        (*frame)(gFrames[gCurrentFrame].count);
+    }
+}
+
+//---------------------- Program Start ------------------------------//
 
 uint8_t gCounter = 0;
 uint8_t gVariable = 0;
 uint16_t gAccumulator = 0;
 
+void frame0(uint16_t* count)
+{
+    drawImage(Logo, count, 0, 0, true, 0);   
+}
+
+void frame1(uint16_t* count)
+{
+    drawImage(Heart, count, 0, 0, true, 0); 
+}
+
 void frame2(uint16_t* count)
 {
-    const uint8_t bpmX = 94;
-    const uint8_t bpmY = 32;
-
-    waitFrame(count);
-    
-    gVariable = random(0, 9);
-    gAccumulator += gVariable;
-    gCounter++;
-
-    arduboy.clear();
-
-    sprites.drawSelfMasked(bpmX, bpmY, Numbers, 8);
-    sprites.drawSelfMasked(bpmX + 5, bpmY, Numbers, gVariable);
-    sprites.drawSelfMasked(bpmX-20, bpmY, BPM, 0);
-
-    arduboy.display();
+    randomizeVariable(&gVariable, 0, 9);
+    nop(count);
 }
 
 void frame3(uint16_t* count)
 {
+    addToVariable(&gAccumulator, gVariable);
+    nop(count);
+}
+
+void frame4(uint16_t* count)
+{
+    addToVariable(&gCounter, 1);
+    nop(count);    
+}
+
+void frame5(uint16_t* count)
+{ 
     const uint8_t bpmX = 94;
     const uint8_t bpmY = 32;
 
-    drawImage(Heart, count);
+    drawImage(Numbers, count, bpmX, bpmY, false, 8);
+    drawImage(Numbers, count, bpmX + 5, bpmY, false, gVariable);
+    drawImage(BPM, count, bpmX - 20, bpmY, false, 0);
+}
+
+void frame6(uint16_t* count)
+{
+    drawImage(Heart, count, 0, 0, true, 0);
     gVariable = random(0, 9);
     gAccumulator += gVariable;
     gCounter++;
 }
 
-void frame4(uint16_t* count)
+void frame7(uint16_t* count)
 {
     nop(count);
     if(*count != 0)
     {
-        *gFrames[2].count = 20;
-        *gFrames[3].count = 20;
+        *gFrames[2].count = 1;  
+        *gFrames[3].count = 1; 
+        *gFrames[4].count = 1;               
+        *gFrames[5].count = 20;
+        *gFrames[6].count = 20;
         gCurrentFrame = 2;
     }
     else
     {
-        gCurrentFrame = 5;
+        gCurrentFrame = 8;
     }
 }
 
@@ -120,23 +156,18 @@ void setup ()
 
     frameInfo info0(180, &frame0);
     frameInfo info1(180, &frame1);
-    frameInfo info2(20, &frame2);
-    frameInfo info3(20, &frame3);
-    frameInfo info4(20, &frame4);
+    frameInfo info2(1, &frame2);
+    frameInfo info3(1, &frame3); 
+    frameInfo info4(1, &frame4);        
+    frameInfo info5(20, &frame5);
+    frameInfo info6(20, &frame6);
+    frameInfo info7(20, &frame7);
     gFrames[0] = info0;
     gFrames[1] = info1;
-    gFrames[2] = info2;
+    gFrames[2] = info2;    
     gFrames[3] = info3;
     gFrames[4] = info4;
-}
-
-#define TOTAL_FRAMES 5
-
-void loop()
-{
-    if(gCurrentFrame < TOTAL_FRAMES)
-    {
-        void (*frame)(uint16_t*) = (void (*)(uint16_t*))gFrames[gCurrentFrame].method;
-        (*frame)(gFrames[gCurrentFrame].count);
-    }
+    gFrames[5] = info5;
+    gFrames[6] = info6; 
+    gFrames[7] = info7;        
 }
